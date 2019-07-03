061d7716424ae50985c39496bb83ccb1b405680a
pwwang <pwwang@pwwang.com>
2019-07-03 09:22:10 -0700

    Add directories to data.

--- remotedata.py
+++ remotedata.py
@@ -1,33 +1,46 @@
+"""Accessing and caching remote data"""
 import warnings
-import requests
 import hashlib
+import shutil
+import json
+import base64
 from datetime import datetime
 from pathlib import Path
+import requests
 from simpleconf import Config
 
-config = Config(with_profile = False)
-config._load(dict(
+CONFIG = Config(with_profile = False)
+CONFIG._load(dict(
 	source   = 'github',
 	cachedir = '/tmp/',
 	repos    = None
 ), '~/.remotedata.yaml', './.remotedata.yaml', 'REMOTEDATA.osenv')
 
-class RemoteData:
+def sha1File(filepath):
+	"""Get sha1 for file"""
+	bufsize = 2**16
+	ret = hashlib.sha1()
+	with open(filepath, 'rb') as fhand:
+		block = fhand.read(bufsize)
+		while len(block) != 0:
+			ret.update(block)
+			block = fhand.read(bufsize)
+	return ret.hexdigest()
 
-	@staticmethod
-	def sha1(filepath):
-		bufsize = 2**16
-		ret = hashlib.sha1()
-		with open(filepath, 'rb') as f:
-			block = f.read(bufsize)
-			while len(block) != 0:
-				ret.update(block)
-				block = f.read(bufsize)
-		return ret.hexdigest()
+def sha1(filepath):
+	filepath = Path(filepath)
+	if not filepath.is_dir():
+		return sha1File(filepath)
+	ret = hashlib.sha1()
+	for path in sorted(filepath.glob('*')):
+		if path.suffix == '.meta':
+			continue
+		ret.update(sha1(path).encode())
+		if path.is_dir():
+			ret.update(path.name.encode())
+	return ret.hexdigest()
 
-	def __init__(self, conf):
-		self.conf     = conf
-		self.cachedir = Path(conf.cachedir)
+class RemoteFile:
 
 	@property
 	def url(self): # pragma: no cover
@@ -40,72 +53,131 @@ class RemoteData:
 			return str(filename.parent / ('.' + filename.name + '.meta'))
 		return '.' + filename + '.meta'
 
-	def remoteMeta(self, filename):
-		meta = self.url.replace('{{file}}', RemoteData.metafile(filename))
-		r = requests.get(meta)
-		if r.status_code != 200:
-			return None, None
-		return r.text.strip().split('|', 1)
+	def __init__(self, conf):
+		self.conf     = conf
+		self.cachedir = Path(conf.cachedir)
 
-	def localMeta(self, filename):
-		meta = self.cachedir / RemoteData.metafile(filename)
-		if not meta.exists() or not (self.cachedir / filename).exists():
+	def sha1(self, filename):
+		return sha1(self.cachedir / filename)
+
+	def remoteMetafile(self, filename):
+		return self.url.replace('{{file}}', RemoteFile.metafile(filename))
+
+	def localMetafile(self, filename):
+		return self.cachedir / RemoteFile.metafile(filename)
+
+	def remoteMetaExists(self, filename):
+		ret = requests.get(self.remoteMetafile(filename))
+		return ret.status_code == 200, ret
+
+	def localMetaExists(self, filename):
+		return self.localMetafile(filename).exists()
+
+	def remoteMetadata(self, filename):
+		rmexists, req = self.remoteMetaExists(filename)
+		if not rmexists:
 			return None, None
-		return meta.read_text().strip().split('|', 1)
+		mtime, sha = req.text.strip().split('|', 1)
+		return int(float(mtime)), sha
+
+	def localMetadata(self, filename):
+		if not self.localMetaExists(filename):
+			return None, None
+		mtime, sha = self.localMetafile(filename).read_text().strip().split('|', 1)
+		return int(float(mtime)), sha
 
 	def isCached(self, filename):
-		local_mtime, local_sha = self.localMeta(filename)
+		local_mtime, local_sha = self.localMetadata(filename)
 		if not local_mtime:
 			return False
-		remote_mtime, remote_sha = self.remoteMeta(filename)
+		remote_mtime, remote_sha = self.remoteMetadata(filename)
 		if not remote_mtime:
 			warnings.warn(
 				'Cannot fetch meta information for %r, will always download it.' % filename)
-			remote_mtime = datetime.now().timestamp()
-		if int(remote_mtime) > int(local_mtime):
+			remote_mtime = int(datetime.now().timestamp())
+		if remote_mtime > local_mtime:
 			return False
 		return local_sha == remote_sha
 
-	def download(self, filename):
-		localfile = self.cachedir / filename
-		fileurl   = self.url.replace('{{file}}', filename)
-		localfile.parent.mkdir(exist_ok=True, parents=True)
-		r = requests.get(fileurl)
-		with localfile.open('wb') as f:
-			f.write(r.content)
+	def updateRemoteMetafile(self, filename): # pragma: no cover
+		raise NotImplementedError()
 
-		meta = self.url.replace('{{file}}', RemoteData.metafile(filename))
-		localmeta = self.cachedir / RemoteData.metafile(filename)
-		r = requests.get(meta)
-		if r.status_code != 200:
-			mtime = datetime.now().timestamp()
-			sha   = RemoteData.sha1(localfile)
-			with localmeta.open('w') as f:
-				r.write(str(mtime) + '|' + sha)
+	def updateLocalMetafile(self, filename, remote = None):
+		with self.localMetafile(filename).open('w') as fmeta:
+			if remote:
+				fmeta.write(remote)
+			else:
+				fmeta.write('%s|%s' % (
+					int((self.cachedir / filename).stat().st_mtime),
+					self.sha1(filename)))
+
+	def download(self, filename):
+		localfile  = self.cachedir / filename
+		remotefile = self.url.replace('{{file}}', filename)
+		# make sure directories have been created
+		localfile.parent.mkdir(exist_ok=True, parents=True)
+		r = requests.get(remotefile)
+		with localfile.open('wb') as fhand:
+			fhand.write(r.content)
+
+		rmexists, req = self.remoteMetaExists(filename)
+		if not rmexists:
+			self.updateLocalMetafile(filename)
+			self.updateRemoteMetafile(filename)
 		else:
-			with localmeta.open('w') as f:
-				f.write(r.text)
+			self.updateLocalMetafile(filename, remote = req.text)
 
 	def get(self, filename):
-		if not self.isCached():
+		if not self.isCached(filename):
 			self.download(filename)
 		return self.cachedir / filename
 
-	def remove(self, filename):
-		"""Remote local cached file
-		Just remove the meta file will work"""
-		metafile = self.cachedir / RemoteData.metafile(filename)
-		if metafile.exists():
-			metafile.unlink()
+class RemoteDir(RemoteFile):
+
+	def listDir(self, filename): # pragma: no cover
+		raise NotImplementedError()
+
+	def download(self, filename):
+		localdir = self.cachedir / filename
+		# make sure local dir is created
+		localdir.mkdir(exists_ok = True, parents = True)
+		paths = self.listDir(filename)
+		for path, ptype in paths:
+			if ptype == 'dir':
+				self.download(path)
+			else:
+				super().download(path)
+
+class RemoteData:
+	"""Base class for remote data"""
+
+	def __init__(self, conf, fileclass = RemoteFile, dirclass = RemoteDir):
+		self.remotefile = fileclass(conf)
+		self.remotedir  = dirclass(conf)
+
+	def gettype(self, filename):
+		return 'file'
+
+	def get(self, filename):
+		if self.gettype(filename) == 'dir':
+			return self.remotedir.get(filename)
+		return self.remotefile.get(filename)
 
 	def clear(self):
-		for path in self.cachedir.glob('*'):
-			if path.isdir():
-				shutil.rmtree(path)
-			else:
-				path.unlink()
+		for path in self.remotefile.cachedir.glob('*'):
+			try:
+				if path.is_dir():
+					shutil.rmtree(path)
+				else:
+					path.unlink()
+			except (PermissionError, OSError):
+				pass
 
-class GithubRemoteData(RemoteData):
+	def remove(self, filename):
+		if self.remotefile.localMetaExists(filename):
+			self.remotefile.localMetafile(filename).unlink()
+
+class GithubRemoteFile(RemoteFile):
 
 	@property
 	def repository(self):
@@ -124,10 +196,47 @@ class GithubRemoteData(RemoteData):
 			raise ValueError('Github remote data needs "repos" to be set.')
 		return 'https://raw.githubusercontent.com/%s/%s/{{file}}' % (self.repository)
 
+	def updateRemoteMetafile(self, filename):
+		# try to upload a metafile
+		repos, branch = self.repository
+		api = 'https://api.github.com/repos/%s/contents/%s' % (repos, filename)
+		metafile = self.localMetafile(filename)
+		requests.put(api, data = dict(
+			message = 'Create metafile for %s' % filename,
+			content = base64.b64encode(metafile.read_bytes()),
+			sha     = sha1(metafile),
+			branch  = branch,
+			committer = {}
+		))
 
-if config.source == 'github':
-	remotedata = GithubRemoteData(config)
-else:
-	remotedata = RemoteData(config)
+class GithubRemoteDir(GithubRemoteFile):
 
+	def listDir(self, filename):
+		repos, branch = self.repository
+		api = 'https://api.github.com/repos/%s/contents/%s' % (repos, filename)
+		req = requests.get(api, params = {'ref': 'branch'})
+		data = json.loads(req.text)
+		ret = []
+		for item in data:
+			ret.append((item['path'], item['type']))
+		return ret
+
+class GithubRemoteData(RemoteData):
+
+	def __init__(self, conf, fileclass=GithubRemoteFile, dirclass=GithubRemoteDir):
+		self.contents = None
+		return super().__init__(conf, fileclass=fileclass, dirclass=dirclass)
+
+	def gettype(self, filename):
+		repos, branch = self.remotefile.repository
+		api = 'https://api.github.com/repos/%s/contents/%s' % (repos, filename)
+		req = requests.get(api, params = {'ref': 'branch'})
+		if req.text[0] == '[':
+			return 'dir'
+		return 'file'
+
+if CONFIG.source == 'github':
+	remotedata = GithubRemoteData(CONFIG)
+else: # pragma: no cover
+	remotedata = RemoteData(CONFIG)
 
--- tests/test_remotedata.py
+++ tests/test_remotedata.py
@@ -1,42 +1,48 @@
 import pytest
+from datetime import datetime
 from pathlib import Path
-from remotedata import remotedata
+from remotedata import remotedata, RemoteData, sha1
 
 
 @pytest.fixture
 def datadir():
 	return Path(__file__).resolve().parent / 'data'
 
-@pytest.fixture
-def rd(tmp_path):
-	remotedata.cachedir = tmp_path
-	remotedata.conf.repos = 'pwwang/remotedata'
-	return remotedata
+@pytest.fixture(scope="function")
+def rdfile(tmp_path):
+	remotedata.remotefile.cachedir = tmp_path
+	remotedata.remotefile.conf.repos = 'pwwang/remotedata'
+	return remotedata.remotefile
 
-def test_remotemeta(datadir, rd):
-	x = rd.remoteMeta('tests/data/file.txt')
+def test_remotemeta(datadir, rdfile):
+	mtime, sha = rdfile.remoteMetadata('tests/data/file.txt')
 	metafile = datadir / '.file.txt.meta'
-	assert x == metafile.read_text().strip().split('|', 1)
+	mtime2, sha2 = metafile.read_text().strip().split('|', 1)
+	assert mtime == int(mtime2)
+	assert sha == sha2
 
-	assert rd.remoteMeta('__not__exists__') == (None, None)
+	assert rdfile.remoteMetadata('__not__exists__') == (None, None)
 
-def test_localmeta(datadir, rd):
-	assert rd.localMeta('__not__exists__') == (None, None)
-	localmetafile = rd.cachedir / 'tests' / 'data' / '.file.txt.meta'
+def test_localmeta(datadir, rdfile):
+	assert rdfile.localMetadata('__not__exists__') == (None, None)
+	localmetafile = rdfile.cachedir / 'tests' / 'data' / '.file.txt.meta'
 	localmetafile.parent.mkdir(exist_ok=True, parents=True)
 	(localmetafile.parent / 'file.txt').write_text('')
 	with open(localmetafile, 'w') as f1, \
 		open(datadir / '.file.txt.meta') as f2:
 		f1.write(f2.read())
 	metafile = datadir / '.file.txt.meta'
-	assert rd.localMeta('tests/data/file.txt') == metafile.read_text().strip().split('|', 1)
+	mtime, sha = rdfile.localMetadata('tests/data/file.txt')
+	mtime2, sha2 = metafile.read_text().strip().split('|', 1)
+	assert mtime == int(mtime2)
+	assert sha == sha2
 
-def test_iscached(datadir, rd):
-	localmetafile = rd.cachedir / 'tests' / 'data' / '.file.txt.meta'
-	localmetafile2 = rd.cachedir / 'tests' / 'data' / '.file2.txt.meta'
+def test_iscached(datadir, rdfile):
+	localmetafile = rdfile.cachedir / 'tests' / 'data' / '.file.txt.meta'
+	localmetafile2 = rdfile.cachedir / 'tests' / 'data' / '.file2.txt.meta'
 	if localmetafile.exists():
 		localmetafile.unlink()
-	assert not rd.isCached('tests/data/file.txt')
+	assert not rdfile.isCached('tests/data/file.txt')
 
 	localmetafile2.parent.mkdir(parents = True, exist_ok = True)
 	(localmetafile2.parent / 'file2.txt').write_text('')
@@ -44,15 +50,58 @@ def test_iscached(datadir, rd):
 		open(datadir / '.file.txt.meta') as f2:
 		f1.write(f2.read())
 	with pytest.warns(UserWarning):
-		assert not rd.isCached('tests/data/file2.txt')
+		assert not rdfile.isCached('tests/data/file2.txt')
 
 	with open(localmetafile, 'w') as f1, \
 		open(datadir / '.file.txt.meta') as f2:
 		f1.write(f2.read())
 	(localmetafile.parent / 'file.txt').write_text('')
-	assert rd.isCached('tests/data/file.txt')
+	assert rdfile.isCached('tests/data/file.txt')
 
-def test_download(datadir, rd):
-	rd.download('tests/data/file.txt')
-	assert (rd.cachedir / 'tests/data/file.txt').read_text() == (datadir / 'file.txt').read_text()
+def test_download(datadir, rdfile):
+	rdfile.download('tests/data/file.txt')
+	assert (rdfile.cachedir / 'tests/data/file.txt').read_text() == (datadir / 'file.txt').read_text()
 
+	rdfile.download('tests/data/nometa.txt')
+	now = datetime.now().timestamp()
+	sha = sha1(rdfile.cachedir / 'tests/data/nometa.txt')
+	mtime, shahash = rdfile.localMetadata('tests/data/nometa.txt')
+	assert now - float(mtime) < 30
+	assert shahash == sha
+
+def test_get_remove(datadir, rdfile, tmp_path):
+	remotedata.clear()
+	filetxt = rdfile.get('tests/data/file.txt')
+	assert filetxt == rdfile.cachedir / 'tests/data/file.txt'
+	assert filetxt.exists()
+	assert rdfile.isCached('tests/data/file.txt')
+	remotedata.remove('tests/data/file.txt')
+	assert not rdfile.isCached('tests/data/file.txt')
+	assert rdfile.cachedir.exists()
+	assert tmp_path.exists()
+
+def test_clear(rdfile):
+	rdfile.get('tests/data/file.txt')
+	nometa = rdfile.get('tests/data/nometa.txt')
+	assert rdfile.isCached('tests/data/file.txt')
+	assert not rdfile.isCached('tests/data/nometa.txt')
+	assert nometa.exists()
+	(rdfile.cachedir / 'file').write_text('')
+	remotedata.clear()
+	assert not rdfile.isCached('tests/data/file.txt')
+	assert not rdfile.isCached('tests/data/nometa.txt')
+
+def test_repository(rdfile):
+	rdfile.conf.repos = ''
+	with pytest.raises(ValueError):
+		rdfile.repository
+	rdfile.conf.repos = 'pwwang/remotedata/develop'
+	assert rdfile.repository == ('pwwang/remotedata', 'develop')
+
+def test_url(rdfile):
+	rdfile.conf.repos = ''
+	with pytest.raises(ValueError):
+		rdfile.url
+
+def test_sha1(datadir):
+	assert sha1(datadir / 'dir') == '6e3d422104ceecf644d494550b7f9d32b8fd48de'
